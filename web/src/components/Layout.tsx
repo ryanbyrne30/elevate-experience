515234bd24ea92929748473c509a79e4afbcf9d8
ryanbyrne30 <ryanjbyrne30@gmail.com>
2022-10-23 10:06:39 -0700

    Admin page

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Nav />
-      <main>{children}</main>
+      <main className="pt-16">{children}</main>
     </>
   );
 }
