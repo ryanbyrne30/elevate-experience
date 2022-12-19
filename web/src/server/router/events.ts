8c64d4bed003be8b0aea2d1efbd48f340307f412
ryanbyrne30 <ryanjbyrne30@gmail.com>
2022-12-19 10:55:21 -0700

    Register trpc route

--- web/src/server/router/events.ts
+++ web/src/server/router/events.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import { createRouter } from "./context";
+import { createProtectedRouter, createRouter } from "./context";
+import { TRPCError } from "@trpc/server";
 
 export const eventsRouter = createRouter()
   .query("getAll", {
@@ -14,31 +15,90 @@ export const eventsRouter = createRouter()
     async resolve({ input, ctx }) {
       return await ctx.prisma.event.findUniqueOrThrow({
         where: { id: input.id },
+        include: {
+          teams: {
+            include: {
+              teamPlayers: true,
+            },
+          },
+        },
       });
-    },
-  })
-  .mutation("create", {
-    input: z.object({
-      name: z.string(),
-      date: z.date(),
-      location: z.string().nullish(),
-      description: z.string().nullish(),
-    }),
-    async resolve({ input, ctx }) {
-      return await ctx.prisma.event.create({
-        data: input,
-      });
-    },
-  })
-  .mutation("update", {
-    input: z.object({
-      id: z.number(),
-      name: z.union([z.undefined(), z.string()]),
-      date: z.union([z.undefined(), z.date()]),
-      location: z.union([z.undefined(), z.string()]),
-      description: z.union([z.undefined(), z.string()]),
-    }),
-    async resolve({ input, ctx }) {
-      return null;
     },
   });
+
+export const protectedEventsRouter = createProtectedRouter().mutation(
+  "register",
+  {
+    input: z.object({
+      eventId: z.number(),
+      guests: z.array(z.string().transform((s) => s.trim())),
+      userIds: z.array(z.string().transform((s) => s.trim())),
+    }),
+    async resolve({ input, ctx }) {
+      const currentUserId: string = ctx.session.user.id;
+      const event = await ctx.prisma.event.findUniqueOrThrow({
+        where: { id: input.eventId },
+        include: {
+          teams: {
+            include: {
+              teamPlayers: true,
+            },
+          },
+        },
+      });
+
+      // get list of previously registered players
+      const previousRegisteredUserIds: string[] = [];
+      const previousRegisteredGuests: string[] = [];
+      event.teams.forEach((team) => {
+        team.guests
+          ?.split(",")
+          .forEach((g) => previousRegisteredGuests.push(g));
+        team.teamPlayers.forEach((p) =>
+          previousRegisteredUserIds.push(p.userId)
+        );
+      });
+
+      // check if user has not registered prior
+      if (previousRegisteredUserIds.includes(currentUserId))
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You have already registered for this event.",
+        });
+
+      // check if all players have not registered prior
+      input.guests.forEach((guest) => {
+        if (previousRegisteredGuests.includes(guest))
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `A guest is already registered for this event with the name '${guest}'. Please register guests with unique names.`,
+          });
+      });
+      input.userIds.forEach((userId) => {
+        if (previousRegisteredUserIds.includes(userId))
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `User '${userId}' has already registered for this event.`,
+          });
+      });
+
+      // create and return team
+      return await ctx.prisma.team.create({
+        data: {
+          eventId: event.id,
+          guests: input.guests.join(","),
+          teamPlayers: {
+            createMany: {
+              data: [
+                { userId: currentUserId },
+                ...input.userIds.map((userId) => ({
+                  userId,
+                })),
+              ],
+            },
+          },
+        },
+      });
+    },
+  }
+);
